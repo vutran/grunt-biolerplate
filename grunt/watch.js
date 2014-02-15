fededa7458f542067c18cf7f22cf9a93b20b871c
Vu Tran <vu@vu-tran.com>
2014-02-14 21:50:42 -0800

    added LESS to grunt watch

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-    src : {
+    default : {
         files : [
             '<%= buildCfg.srcPath %>/js/**/*.js',
-            '<%= buildCfg.srcPath %>/stylesheets/**/*.{styl,sass,scss}'
+            '<%= buildCfg.srcPath %>/stylesheets/**/*.{less,styl,sass,scss}'
         ],
         tasks : ['build']
-    }
+    },
 
 };
